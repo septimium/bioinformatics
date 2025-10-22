082946db52aab69f2c9113971dca8625fa70d1d9
septimium <septimiutg@gmail.com>
2025-10-22 12:29:37 +0300

    assign1 - lab 4

---
+++ Project_L4/assign1.py
@@ -0,0 +1,42 @@
+# Implement an app that converts the coding region of a gene into an amino acid sequence. Use the genetic code table.
+
+CODE_TABLE = {
+    'UUU': 'Phe', 'UUC': 'Phe', 'UUA': 'Leu', 'UUG': 'Leu',
+    'UCU': 'Ser', 'UCC': 'Ser', 'UCA': 'Ser', 'UCG': 'Ser',
+    'UAU': 'Tyr', 'UAC': 'Tyr', 'UAA': 'Stop', 'UAG': 'Stop',
+    'UGU': 'Cys', 'UGC': 'Cys', 'UGA': 'Stop', 'UGG': 'Trp',
+    'CUU': 'Leu', 'CUC': 'Leu', 'CUA': 'Leu', 'CUG': 'Leu',
+    'CCU': 'Pro', 'CCC': 'Pro', 'CCA': 'Pro', 'CCG': 'Pro',
+    'CAU': 'His', 'CAC': 'His', 'CAA': 'Gln', 'CAG': 'Gln',
+    'CGU': 'Arg', 'CGC': 'Arg', 'CGA': 'Arg', 'CGG': 'Arg',
+    'AUU': 'Ile', 'AUC': 'Ile', 'AUA': 'Ile', 'AUG': 'Met',
+    'ACU': 'Thr', 'ACC': 'Thr', 'ACA': 'Thr', 'ACG': 'Thr',
+    'AAU': 'Asn', 'AAC': 'Asn', 'AAA': 'Lys', 'AAG': 'Lys',
+    'AGU': 'Ser', 'AGC': 'Ser', 'AGA': 'Arg', 'AGG': 'Arg',
+    'GUU': 'Val', 'GUC': 'Val', 'GUA': 'Val', 'GUG': 'Val',
+    'GCU': 'Ala', 'GCC': 'Ala', 'GCA': 'Ala', 'GCG': 'Ala',
+    'GAU': 'Asp', 'GAC': 'Asp', 'GAA': 'Glu', 'GAG': 'Glu',
+    'GGU': 'Gly', 'GGC': 'Gly', 'GGA': 'Gly', 'GGG': 'Gly',
+}
+
+def translate(sequence: str) -> str:
+    result = []
+    sequence = sequence.upper()
+    for i in range(0, len(sequence), 3):
+        code = sequence[i:i+3]
+        if len(code) < 3:
+            break
+        temp = CODE_TABLE.get(code, 'X')
+        if temp == 'Stop':
+            break
+        result.append(temp)
+    return "".join(result)
+
+if __name__ == "__main__":
+    test = input("Sequence: ")
+    result = translate(test)
+    print(result)
+    
+
+
+   
